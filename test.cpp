b0b6a15b458e03426f5c40d0ff2b67d271c8e99d
Kril Allen <15674734251@163.com>
2018-04-28 16:04:29 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,11 +8,10 @@ using namespace cv;
 
 int main(int argc, char *argv[])
 {
-	CvCapture* capture = cvCaptureFromAVI("./video_avi/store_224*224/testvideo_3_1.avi");
+	CvCapture* capture = cvCaptureFromAVI("video_route");
 	int i = 0;
 	IplImage* img = 0;
 	char image_name[25];
-	//const char address ="/home/linhan/huang/test/restore"
 	cvNamedWindow( "vivi");
 	//读取和显示
 	while(1)
@@ -25,12 +24,13 @@ int main(int argc, char *argv[])
 		//char key = cvWaitKey(20);
 		cvWaitKey(20);
 		//eidt string
-		sprintf(image_name, "%s%d%s", "/home/linhan/huang/project/test/restore/image-shi-224*224/", ++i, ".jpg");//保存的图片名
+		sprintf(image_name, "%s%d%s", "image_route", ++i, ".jpg");//保存的图片名
 		cvSaveImage( image_name, img); //保存一帧图片
 		//cvSaveImage(address, img ,0);
 	}
 
 	cvReleaseCapture(&capture);
+	//显示窗口的名称
 	cvDestroyWindow("vivi");
 
 	return 0;
